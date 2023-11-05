692d033c5bf52d83a424ea5795d7339f231e3ff8
aibn <45821186+Eternity-AIBN@users.noreply.github.com>
2023-11-05 14:53:32 +0800

    修改8192为81920

--- src/observer/sql/parser/lex_sql.h
+++ src/observer/sql/parser/lex_sql.h
@@ -361,7 +361,7 @@ static int yy_flex_strlen ( const char * , yyscan_t yyscanner);
 /* On IA-64, the buffer size is 16k, not 8k */
 #define YY_READ_BUF_SIZE 16384
 #else
-#define YY_READ_BUF_SIZE 8192
+#define YY_READ_BUF_SIZE 81920
 #endif /* __ia64__ */
 #endif
 
